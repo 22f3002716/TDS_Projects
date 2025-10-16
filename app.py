123f62130b3ed25cc6d20d2fe67d2382ff89d909
Karthikeyan S <22f3002716@ds.study.iitm.ac.in>
2025-10-16 09:23:45 +0400

    7th Commit for Evaluation URL server

---
+++ app.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI, Request
+
+app = FastAPI()
+
+@app.post("/")
+async def echo(request: Request):
+    body = await request.body()
+    headers = dict(request.headers)
+    return {
+        "method": request.method,
+        "headers": headers,
+        "body": body.decode("utf-8")  # If expecting text input
+    }
